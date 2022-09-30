9a5e6efa1a3335520d322bee562541513f6c2386
Xavi González Mur <xavier.gmur@students.salle.url.edu>
2022-09-30 15:54:17 +0200

    Adding styles and loops

--- src/App.vue
+++ src/App.vue
@@ -17,24 +17,57 @@ export default {
 <template>
   <div id="header"></div>
   <div id="body">
-    <div class="task">
-      <div>{{tasks[0].title}}</div>
-      <div>{{tasks[0].description}}</div>
-      <div>{{tasks[0].priority}}</div>
-    </div>
-    <div class="task">
-      <div>{{tasks[1].title}}</div>
-      <div>{{tasks[1].description}}</div>
-      <div>{{tasks[1].priority}}</div>
-    </div>
-    <div class="task">
-      <div>{{tasks[2].title}}</div>
-      <div>{{tasks[2].description}}</div>
-      <div>{{tasks[2].priority}}</div>
+    <div class="task" v-for="task in tasks">
+      <span><input type="checkbox" name="selected" class="selection"></span>
+      <span>
+        <input v-model="task.title" placeholder="{{task.title}}" class="task-title">
+        <input v-model="task.description" placeholder="{{task.description}}" class="task-description">
+      </span>
+      <span>
+        <select v-model="task.priority" placeholder="{{task.priority}}" class="task-priority"></select>
+      </span>
     </div>
   </div>
 </template>
 
 <style>
-  
+.task {
+  border: 1px dotted black;
+  border-radius: 10px;
+  /*width: 300px;*/
+  display: flex;
+  margin-bottom: 2px;
+  padding: 5px;
+}
+
+.task-title {
+  display: block;
+  font-weight: bold;
+  font-size: x-small;
+  font-style: italic;
+}
+
+.task-description {
+  display: block;
+  color: darkgrey;
+}
+
+.task-priority {
+  /*float: right;*/
+}
+
+.selection {
+  border-radius: 50%;
+  border: 1px solid #ddd;
+  outline: none;
+  appearance: none;
+  width: 2em;
+  height: 2em;
+  cursor: pointer;
+  margin: 5px;
+}
+
+input {
+  border: 0;
+}
 </style>
