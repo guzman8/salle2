733239065b5a76d1843b2aec67a1b92a8698ffbc
Guzmán <usuario@usuarios-mbp.lan>
2022-10-01 14:32:58 +0200

    new layout

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,12 @@ export default {
 </script>
     
 <template>
-  <div id="header"></div>
-  <div id="body">
+
+<div class="grid-container">
+  <div class="header">My tasks</div>
+  <div class="menu">Task folders</div>
+  <div class="tasks">list of tasks
+
     <div :class="['task', 'priority-' + task.priority]" v-for="(task, index) in tasks" :key="index">
       <span>
         <input type="checkbox" name="selected" class="task-selection">
@@ -36,14 +40,18 @@ export default {
       </span>
     </div>
   </div>
+
+  <div class="users">users</div>
+  <div class="footer">@Salle limited inc</div>
+  <div class="options">add delete modify</div>
+ 
+
+</div>
+
+  
 </template>
 
 <style>
-body {
-  display: flex;
-  align-items: center;
-}
-
 span {
   margin: 3px;
 }
@@ -107,4 +115,35 @@ span {
 input {
   border: 0;
 }
+
+.header { grid-area: header; }
+.menu { grid-area: menu;  }
+.tasks { grid-area: main;  }
+.users { grid-area: right; }
+.footer { grid-area: footer; }
+.options { grid-area: options;  }
+
+.grid-container {
+  width: 100%;
+	grid-template-rows:50px 50px 1fr 50px;
+    grid-template-columns: 1.5fr 3fr 2fr;
+  display: grid;
+  grid-template-areas:
+    'header header header'
+    'menu options right'
+    'menu main right'
+    'menu footer footer';
+  gap: 10px;
+  background-color: #b6b8b9;
+  padding: 10px;
+  height: 100vh;
+}
+
+.grid-container > div {
+  background-color: rgba(255, 255, 255, 0.8);
+  text-align: center;
+  padding: 20px 0;
+  font-size: 30px;
+}
+
 </style>
