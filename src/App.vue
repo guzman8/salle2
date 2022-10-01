0cf5b69aa1fc569eddca7597aae602de2324274b
Xavi González Mur <xavier.gmur@students.salle.url.edu>
2022-10-01 21:20:48 +0200

    Added extra hardcoded data unordered Removed div of header and body Select's options are populated using v-for Aesthetic changes

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@ export default {
       tasks: [
         { title: "Title 1", description: "Description 1", priority: "high", completed: false },
         { title: "Title 2", description: "Description 2", priority: "medium", completed: false },
-        { title: "Title 3", description: "Description 3", priority: "low", completed: false }]
+        { title: "Title 3", description: "Description 3", priority: "low", completed: false },
+        { title: "Title 6", description: "Description 6", priority: "high", completed: false },
+        { title: "Title 4", description: "Description 4", priority: "low", completed: false },
+        { title: "Title 5", description: "Description 5", priority: "", completed: false }],
+      priorityValues: ["", "low", "medium", "high"],
     };
   },
 
@@ -16,34 +20,23 @@ export default {
 </script>
     
 <template>
-  <div id="header"></div>
-  <div id="body">
-    <div :class="['task', 'priority-' + task.priority]" v-for="(task, index) in tasks" :key="index">
-      <span>
-        <input type="checkbox" name="selected" class="task-selection">
-      </span>
-      <span class="task-body">
-        <input v-model="task.title" placeholder="{{task.title}}" class="task-title">
-        <input v-model="task.description" placeholder="{{task.description}}" class="task-description">
-      </span>
-      <span>
-        <select v-model="task.priority" class="task-priority" title="Priority">
-          <option>(none)</option>
-          <option>low</option>
-          <option>medium</option>
-          <option>high</option>
-        </select>
-      </span>
-    </div>
+  <div :class="['task', 'priority-' + task.priority]" v-for="(task, index) in tasks" :key="index">
+    <span>
+      <input type="checkbox" name="selected" class="task-selection">
+    </span>
+    <span class="task-body">
+      <input v-model="task.title" placeholder="{{task.title}}" class="task-title">
+      <input v-model="task.description" placeholder="{{task.description}}" class="task-description">
+    </span>
+    <span>
+      <select v-model="task.priority" class="task-priority" title="Priority">
+        <option v-for="priority in priorityValues">{{priority}}</option>
+      </select>
+    </span>
   </div>
 </template>
 
 <style>
-body {
-  display: flex;
-  align-items: center;
-}
-
 span {
   margin: 3px;
 }
@@ -61,7 +54,7 @@ span {
 }
 
 .task {
-  border: 1px solid black;
+  border: 1px solid #4F4F4F;
   border-radius: 10px;
   /*width: 300px;*/
   display: flex;
@@ -86,6 +79,8 @@ span {
   display: block;
   color: #4F4F4F;
   width: 100%;
+  border: 1px solid #f2f2f2;
+  border-radius: 3px;
 }
 
 .task-priority {
@@ -105,6 +100,7 @@ span {
 }
 
 input {
-  border: 0;
+  border: 1px solid #f2f2f2;
+  border-radius: 3px;
 }
 </style>
